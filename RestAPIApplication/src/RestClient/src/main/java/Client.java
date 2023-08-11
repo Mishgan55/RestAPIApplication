4c0a75285251fc8cdc299dea4655a72fe5f04e8c
mishgan55 <vorota.mogilev@gmail.com>
2023-08-11 18:56:58 +0300

    Created request for creating new sensor and 1000 new measurements

--- RestAPIApplication/src/RestClient/src/main/java/Client.java
+++ RestAPIApplication/src/RestClient/src/main/java/Client.java
@@ -5,13 +5,22 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Client {
 
     public static void main(String[] args) {
-        String nameSensor="sensor3";
+        final String nameSensor="main-sensor";
 
         createSensor(nameSensor);
+
+        Random random=new Random();
+        Double maxTemperate=45.0;
+        for (int i = 0; i < 5000; i++) {
+            System.out.println(i);
+            createMeasurement(random.nextDouble()*maxTemperate,random.nextBoolean(),nameSensor);
+        }
+
 
 
 
@@ -25,6 +34,17 @@ public class Client {
         postRequest(map,url);
     }
 
+    private static void createMeasurement(Double value,Boolean raining,String sensorName){
+        Map<String,Object> map= new HashMap<>();
+        String url="http://localhost:8080/measurements/add";
+        map.put("value",value);
+        map.put("raining",raining);
+        map.put("sensor", Map.of("name",sensorName));
+
+        postRequest(map,url);
+
+    }
+
     private static void postRequest(Map<String, Object> map, String url) {
         RestTemplate restTemplate=new RestTemplate();
 
@@ -34,7 +54,7 @@ public class Client {
         HttpEntity<Map<String, Object>> request= new HttpEntity<>(map,httpHeaders);
         try{
         restTemplate.postForEntity(url,request,String.class);
-            System.out.println("Measurement is sucseed");
+            System.out.println("Operation is sucseed");
         }catch (Exception e){
             System.out.println("ERROR");
             e.getMessage();
